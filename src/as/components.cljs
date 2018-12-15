ac0e1ae34574d3fe81e2d6a43ae970f271ea37c6
Aron Strandberg <aron@aronstrandberg.com>
2018-12-15 17:26:09 +0100

    feat: add some basic content

---
+++ src/as/components.cljs
@@ -0,0 +1,23 @@
+(ns as.components)
+
+(defn portrait []
+  [:img {:src "/images/me.jpg"}])
+
+(defn header []
+  [:h1 "Aron Strandberg"])
+
+(defn about []
+  [:p "I'm a software developer from Stockholm, Sweden. I'm currently pursuing a Master's degree in computer science at the Royal Institute of Technology. Besides programming, I'm interested in fashion, photography, and bartending."])
+
+(defn heart []
+  [:span {:dangerouslySetInnerHTML {:__html "&hearts;"}}])
+
+(defn footer []
+  [:p "Made with ", [heart], " in Stockholm"])
+
+(defn home-page []
+  [:div
+    [portrait]
+    [header]
+    [about]
+    [footer]])
--- src/as/core.cljs
+++ src/as/core.cljs
@@ -1,15 +1,7 @@
 (ns as.core
     (:require
-      [reagent.core :as r]))
-
-;; -------------------------
-;; Views
-
-(defn home-page []
-  [:div [:h2 "Welcome to Reagent"]])
-
-;; -------------------------
-;; Initialize app
+      [reagent.core :as r]
+      [as.components :refer [home-page]]))
 
 (defn mount-root []
   (r/render [home-page] (.getElementById js/document "app")))
