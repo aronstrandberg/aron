63c9a09507456fa5ccd57cf55c00df1db8c96bdb
Aron Strandberg <aron@aronstrandberg.com>
2022-04-25 16:15:38 +0200

    style: run `cljfmt`

--- src/as/components.cljs
+++ src/as/components.cljs
@@ -1,7 +1,7 @@
 (ns as.components
   (:require
-    [reagent.core :as r]
-    [cljss.reagent :refer-macros [defstyled]]))
+   [reagent.core :as r]
+   [cljss.reagent :refer-macros [defstyled]]))
 
 (def text-color "#f8f9f8")
 (def background "#f92b28")
@@ -65,17 +65,17 @@
 (defn work []
   [section
    [heading "Work"]
-   [text "Currently, I'm working with digital health care at " [:a {:href "http://kry.se"} "KRY"] ". Previously, I've worked with financial APIs at "[:a {:href "http://www.trioptima.com"} "TriOptima"]", and university recruitment solutions at "[:a {:href "http://sqore.com"} "Sqore"]". Besides software development, I've also worked as a bartender, art director, and team leader at DKM, and as a photographer at "[:a {:href "http://osqledaren.se"} "Osqledaren"]"."]])
+   [text "Currently, I'm working with digital health care at " [:a {:href "http://kry.se"} "KRY"] ". Previously, I've worked with financial APIs at " [:a {:href "http://www.trioptima.com"} "TriOptima"] ", and university recruitment solutions at " [:a {:href "http://sqore.com"} "Sqore"] ". Besides software development, I've also worked as a bartender, art director, and team leader at DKM, and as a photographer at " [:a {:href "http://osqledaren.se"} "Osqledaren"] "."]])
 
 (defn studs []
   [section
    [heading "Projects — Studs"]
-   [text "I served as Art Director for the student project "[:a {:href "http://studieresan.se"} "Studs"]" in 2018. As part of the project's design team, I helped develop the graphical profile, designed print work, and designed and developed a React frontend for " [:a {:href "http://studieresan.se"} "studieresan.se"]"."]])
+   [text "I served as Art Director for the student project " [:a {:href "http://studieresan.se"} "Studs"] " in 2018. As part of the project's design team, I helped develop the graphical profile, designed print work, and designed and developed a React frontend for " [:a {:href "http://studieresan.se"} "studieresan.se"] "."]])
 
 (defn izumi []
   [section
    [heading "Projects — Izumi"]
-   [text [:a {:href "http://izumi.netlify.com"} "Izumi"]" is a Tabata timer. Unable to find a decent Tabata timer for HIIT training, I set out to create my own. Izumi is built using React and Redux."]])
+   [text [:a {:href "http://izumi.netlify.com"} "Izumi"] " is a Tabata timer. Unable to find a decent Tabata timer for HIIT training, I set out to create my own. Izumi is built using React and Redux."]])
 
 (defn footer []
   [section
--- src/as/core.cljs
+++ src/as/core.cljs
@@ -1,7 +1,7 @@
 (ns as.core
-    (:require
-      [reagent.core :as r]
-      [as.components :refer [home-page]]))
+  (:require
+   [reagent.core :as r]
+   [as.components :refer [home-page]]))
 
 (defn mount-root []
   (r/render [home-page] (.getElementById js/document "app")))
